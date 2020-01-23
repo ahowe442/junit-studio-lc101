fad412fdd7c7b39c9fd2128627492e28fa21f85e
Abby Howe <howe.abigail@gmail.com>
2020-01-22 19:52:11 -0600

    Created tests and updated code to pass each of 12 tests

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -11,6 +12,49 @@ public class BalancedBracketsTest {
     public void emptyTest() {
         assertEquals(true, true);
     }
+    @Test
+    public void NotNull(){
+        assertNotNull(BalancedBrackets.hasBalancedBrackets(""));
+    }
+    @Test
+    public void onlyBracketsReturnsTrue(){
+        assertEquals(true, BalancedBrackets.hasBalancedBrackets("[]"));
+    }
 
-
+    @Test
+    public void bracketsAndCharsReturnsTrue(){
+        assertEquals(true, BalancedBrackets.hasBalancedBrackets("[uioio]"));
+    }
+    @Test
+    public void stringOnTheOutsideOfBalancedBrackets(){
+        assertEquals(true, BalancedBrackets.hasBalancedBrackets("asdf[]"));
+    }
+    @Test
+    public void curlyBracketsReturnFalse(){
+        assertFalse( BalancedBrackets.hasBalancedBrackets("{}"));
+    }
+    @Test
+    public void onlyOpenBracketReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+    @Test
+    public void backwardBracketsReturnFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+    @Test
+    public void onlyClosingBracketReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+    @Test
+    public void arrowBracketsReturnFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("<>"));
+    }
+    @Test
+    public void backFacingBracketsWithTextBetweenReturnFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Code["));
+    }
+    @Test
+    public void hasBracketsBeforeString(){
+        assertEquals(true, BalancedBrackets.hasBalancedBrackets("[]LaunchCode"));
+    }
 }
